75a2fda4ebc94a9393e69b1c644c5a4e9e07ba00
Liza <ye.haponenko@ukma.edu.ua>
2025-06-05 16:59:13 +0300

    sidebar design

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Search, SlidersHorizontal } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,15 +7,21 @@ function Sidebar({ isOpen }) {
         <div className={`sidebar-container ${isOpen ? 'open' : ''}`}>
             <div className="custom-sidebar">
                 <div className="menu">
-                    <div className="menu-title">Menu</div>
-                    <Link to="/" className="menu-link">
-                        <img src="/assets/images/settings.png" alt="icon" className="icon" />
-                        <span className="label">Something</span>
-                    </Link>
-                    <Link to="/" className="menu-link">
-                        <img src="/assets/images/settings.png" alt="icon" className="icon" />
-                        <span className="label">Something</span>
-                    </Link>
+                    <div className="search-container">
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                            className="search-input"
+                        />
+                        <button className="search-button">
+                            <Search size={25} className="search-icon" />
+                        </button>
+                    </div>
+                    <div className="menu-link">
+                        <SlidersHorizontal size={25} />
+                        <span className="label">Filter</span>
+
+                    </div>
                 </div>
                 <div className="menu">
                     <div className="menu-title">Links</div>
