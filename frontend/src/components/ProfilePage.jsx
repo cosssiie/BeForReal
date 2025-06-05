6d853249f7a3411cab78892dea95cbc51579b2a3
Liza <ye.haponenko@ukma.edu.ua>
2025-06-05 22:16:17 +0300

    profile page

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -3,7 +3,32 @@ import React from 'react';
 function ProfilePage() {
     return (
         <div className="profile-container">
-        
+            <div className="profile">
+                <div className="profile-header">
+                    <div className="profile-photo">
+                        <img src="" alt="" />
+                    </div>
+                    <div className="profile-info">
+                        <div className="personal-info">
+                            <span className="nickname">Someone</span>
+                            <div className="profile-buttons">
+                                <button className="change-profile">Change Profile</button>
+                                <button className="change-profile">Change Profile</button>
+                            </div>
+                        </div>
+                        <div className="statistics">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="profile-posts">
+                    <nav>
+                        <ul>
+
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </div>
     );
 }
